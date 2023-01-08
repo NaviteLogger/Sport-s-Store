69e73ecf4d7f7ca71c2b9e946d16b5af093f2f23
NaviteLogger5547 <66165957+NaviteLogger@users.noreply.github.com>
2023-01-08 01:17:41 +0100

    Added a few starting options in Program.cs

--- Program.cs
+++ Program.cs
@@ -18,12 +18,12 @@ namespace SportsStore
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.MapControllerRoute(
