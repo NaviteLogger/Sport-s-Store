4c45fbc168f2e3ec369315bd6ed11b24d519aaf2
NaviteLogger5547 <66165957+NaviteLogger@users.noreply.github.com>
2023-01-08 22:35:48 +0100

    Installed SqlServer package

--- Models/Products.cs
+++ Models/Products.cs
@@ -1,6 +1,17 @@
-﻿namespace SportsStore.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportsStore.Models
 {
     public class Products
     {
+        public long ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
     }
 }
