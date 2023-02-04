c07c0fa68a80900e4abe2393c6916930231f6790
NaviteLogger <kacprzakmarek92@gmail.com>
2023-02-04 12:29:48 +0100

    Test commit after a while to verify the integrity of files

--- Models/Products.cs
+++ Models/Products.cs
@@ -5,12 +5,12 @@ namespace SportsStore.Models
     public class Products
     {
         public long ProductID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
 
         [Column(TypeName = "decimal(8, 2)")]
         public decimal Price { get; set; }
 
-        public string Category { get; set; }
+        public string? Category { get; set; }
     }
 }
