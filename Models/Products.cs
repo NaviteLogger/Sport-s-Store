ce24d4628aa00f2654749641a5d1c6fbaab7321d
NaviteLogger <kacprzakmarek92@gmail.com>
2023-02-13 21:38:44 +0100

    Integrated PostSharp into this project

--- Models/Products.cs
+++ Models/Products.cs
@@ -2,13 +2,13 @@
 
 namespace SportsStore.Models
 {
-    public class Products
+    public class Products //provides a template for the Products table in the database
     {
         public long ProductID { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
 
-        [Column(TypeName = "decimal(8, 2)")]
+        [Column(TypeName = "decimal(8, 2)")] //provides the data type for the Price column in the SQL database
         public decimal Price { get; set; }
 
         public string? Category { get; set; }
