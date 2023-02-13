26eb5f2e4fbfb65bd3a8562ea44602efc9da6726
NaviteLogger <kacprzakmarek92@gmail.com>
2023-02-13 21:27:38 +0100

    Added comments to the StoreDbContext.cs class

--- Models/StoreDbContext.cs
+++ Models/StoreDbContext.cs
@@ -2,9 +2,9 @@
 
 namespace SportsStore.Models
 {
-    public class StoreDbContext : DbContext
+    public class StoreDbContext : DbContext //DBContext is a class in the Entity Framework Core namespace, provides the basic functionality for connecting to a database and mapping objects to database tables
     {
-        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { }
-        public DbSet<Products> Products { get; set; }
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { } //provides the connection string to the database
+        public DbSet<Products> Products { get; set; } //provides the access to the Products table in the database
     }
 }
